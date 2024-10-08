663b0aa0571ecf4153e77da68259c378beeb8d44
gacherevatenko <gacherevatenko@domclick.ru>
2024-10-08 16:43:20 +0300

    tests

--- itgram-be/itgram-biz/src/commonTest/kotlin/validation/BaseBizValidationTest.kt
+++ itgram-be/itgram-biz/src/commonTest/kotlin/validation/BaseBizValidationTest.kt
@@ -1,11 +1,19 @@
 package ru.itgram.biz.validation
 
+import PublicationRepoInMemory
+import PublicationRepoInitialized
 import ru.itgram.biz.MkplPublicationProcessor
 import ru.itgram.common.MkplCorSettings
 import ru.itgram.common.models.MkplCommand
 
 abstract class BaseBizValidationTest {
     protected abstract val command: MkplCommand
-    private val settings by lazy { MkplCorSettings() }
+    private val repo = PublicationRepoInitialized(
+        repo = PublicationRepoInMemory(),
+        initObjects = listOf(
+            MkplPublicationStub.get(),
+        ),
+    )
+    private val settings by lazy { MkplCorSettings(repoTest = repo) }
     protected val processor by lazy { MkplPublicationProcessor(settings) }
 }
