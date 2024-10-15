fd1f9f9ae6d3174de94744100b33b498f1bc7d48
gacherevatenko <gacherevatenko@domclick.ru>
2024-10-15 11:14:52 +0300

    restTest

--- itgram-tests/itgram-e2e-be/src/test/kotlin/test/AccRestTest.kt
+++ itgram-tests/itgram-e2e-be/src/test/kotlin/test/AccRestTest.kt
@@ -1,5 +1,17 @@
 package test
 
+import fixture.BaseFunSpec
+import fixture.client.RestClient
+import fixture.docker.DockerCompose
+import io.kotest.core.annotation.Ignored
+import test.action.v1.toV1
+
 enum class TestDebug {
     STUB, PROD, TEST
 }
+
+@Ignored
+open class AccRestTestBaseFull(dockerCompose: DockerCompose, debug: TestDebug = TestDebug.STUB) : BaseFunSpec(dockerCompose, {
+    val restClient = RestClient(dockerCompose)
+    testApiV1(restClient, prefix = "rest ", debug = debug.toV1())
+})
