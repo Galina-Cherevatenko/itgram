94b3aa33805a0248626fa6df0da277cc63044d15
gacherevatenko <gacherevatenko@domclick.ru>
2024-11-01 22:46:19 +0300

    spring

--- itgram-be/itgram-app-spring/src/main/kotlin/config/PublicationConfig.kt
+++ itgram-be/itgram-app-spring/src/main/kotlin/config/PublicationConfig.kt
@@ -1,9 +1,12 @@
 package ru.itgram.app.spring.config
 
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import PublicationRepoInMemory
 import PublicationRepoStub
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import ru.itgram.backend.repo.postgresql.RepoPublicationSql
 import ru.itgram.biz.MkplPublicationProcessor
 import ru.itgram.common.MkplCorSettings
 import ru.itgram.common.repo.IRepoPublication
@@ -12,7 +15,10 @@ import ru.itgram.logging.jvm.mpLoggerLogback
 
 @Suppress("unused")
 @Configuration
-class PublicationConfig  {
+class PublicationConfig(val postgresConfig: PublicationConfigPostgres) {
+
+    val logger: Logger = LoggerFactory.getLogger(PublicationConfig::class.java)
+
     @Bean
     fun processor(corSettings: MkplCorSettings) = MkplPublicationProcessor(corSettings = corSettings)
 
@@ -23,7 +29,9 @@ class PublicationConfig  {
     fun testRepo(): IRepoPublication = PublicationRepoInMemory()
 
     @Bean
-    fun prodRepo(): IRepoPublication = PublicationRepoInMemory()
+    fun prodRepo(): IRepoPublication = RepoPublicationSql(postgresConfig.psql).apply {
+        logger.info("Connecting to DB with ${this}")
+    }
 
     @Bean
     fun stubRepo(): IRepoPublication = PublicationRepoStub()
